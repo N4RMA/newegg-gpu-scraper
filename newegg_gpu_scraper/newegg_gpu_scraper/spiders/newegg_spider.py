7d7da45970c9470ac76ff8eab10955ce3604634d
N4RMA <manar-yacine@outlook.com>
2023-05-05 21:49:33 -0400

    scrapes name, price and url  of first page products

--- newegg_gpu_scraper/newegg_gpu_scraper/spiders/newegg_spider.py
+++ newegg_gpu_scraper/newegg_gpu_scraper/spiders/newegg_spider.py
@@ -4,7 +4,26 @@ import scrapy
 class NeweggSpiderSpider(scrapy.Spider):
     name = "newegg_spider"
     allowed_domains = ["newegg.ca"]
-    start_urls = ["http://newegg.ca/"]
+    start_urls = ["https://www.newegg.ca/GPUs-Video-Graphics-Cards/SubCategory/ID-48"]
 
     def parse(self, response):
-        pass
+        products = response.xpath("//div[contains(@class, 'item-cell')]")
+
+        for product in products:
+            name = product.xpath(".//a[contains(@class, 'item-title')]/text()").get()
+            price_whole_part = product.xpath(".//li[contains(@class, 'price-current')]/strong/text()").get()
+            price_fraction_part = product.xpath(".//li[contains(@class, 'price-current')]/sup/text()").get()
+            price = f'{price_whole_part}{price_fraction_part}'
+            url = product.xpath(".//a[contains(@class, 'item-title')]/@href").get()
+
+            # Yield the extracted data as a dictionary
+            yield {
+                'name': name,
+                'price': price,
+                'url': url,
+            }
+
+        # Locate and follow the next page link, if available
+        next_page_url = response.xpath("//a[contains(@class, 'next')]/@href").get()
+        if next_page_url:
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
