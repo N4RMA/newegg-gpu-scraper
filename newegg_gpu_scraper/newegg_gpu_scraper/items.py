ec173be3bddfa1188eb056de78d0a26e4ffd498f
N4RMA <manar-yacine@outlook.com>
2023-05-06 23:08:36 -0400

    defined items

--- newegg_gpu_scraper/newegg_gpu_scraper/items.py
+++ newegg_gpu_scraper/newegg_gpu_scraper/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class NeweggGpuScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class GpuItem(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
+    url = scrapy.Field()
